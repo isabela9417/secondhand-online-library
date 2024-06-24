4a15d62b9bb7526bc3a3737cf058d4ae17caf6e6
isabela9417 <isabelachana@gmail.com>
2024-06-24 15:39:29 +0200

    added css to the login page

--- templates/register.php
+++ templates/register.php
@@ -9,24 +9,27 @@
 
 	<!-- icons link -->
 	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-	<!-- font link -->
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-	<!-- css -->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- font awesome cdn link -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+	<!--custom css -->
+	<link rel="stylesheet" href="../css/style.css">
+
 </head>
 <body>
 <div class="form-container">
-	<h3>Register Your Account</h3>
 	<form action="" method="post">
+		<h3>Register Your Account</h3>
 		<input type="text" name="name" placeholder="Enter Your Name" required class="box">
 		<input type="email" name="email" placeholder="Enter Your Email Address" required class="box">
-		<input type="pasword" name="pasword" placeholder="Enter Your Password" required class="box">
-		<input type="pasword1" name="pasword1" placeholder="Confirm Your Password">
-		<select name="user_type">
+		<input type="password" name="password" placeholder="Enter Your Password" required class="box">
+		<input type="password" name="password1" placeholder="Confirm Your Password" required class="box">
+		<select name="user_type" class="box">
 			<option value="user">Student</option>
 			<option value="admin">Admin</option>
 		</select>
-		<input type="submit" name="submit" value="Register Now" class="btn"><p>Already have an account? <a href="login.php"></a></p>
+		<input type="submit" name="submit" value="Register Now" class="btn"><p>Already have an account? <a href="login.php">Login Now</a></p>
 	</form>
 </div>
 </body>
